d367bf2874c6af884e68d659c2e6fb89f1c24a4f
Julian Pasquale <julianagustin78@gmail.com>
2021-01-11 11:28:50 -0300

    Changes stylesheets location

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -17,27 +17,15 @@ class ContactsController < ApplicationController
   end
 
   def activate
-    @contacts.update(active: true)
+    update_status(true)
 
-    render(
-      template: false,
-      partial: 'contacts/contact_row',
-      collection: @contacts,
-      as: :contact,
-      locals: { include_checkbox: true }
-    )
+    render_contact_row
   end
 
   def deactivate
-    @contacts.update(active: false)
+    update_status(false)
 
-    render(
-      template: false,
-      partial: 'contacts/contact_row',
-      collection: @contacts,
-      as: :contact,
-      locals: { include_checkbox: true }
-    )
+    render_contact_row
   end
 
   private
@@ -52,5 +40,29 @@ class ContactsController < ApplicationController
 
   def find_multiple_contacts
     @contacts = Contact.where(id: params[:ids])
+  end
+
+  def update_status(status)
+    Contact.where(id: params[:ids]).update(active: status)
+  end
+
+  def render_contact_row
+    render(
+      template: false,
+      partial: 'contacts/contact_row',
+      collection: Contact.all,
+      as: :contact,
+      locals: {
+        include_checkbox: true,
+        changed_contacts: changed_contacts
+      }
+    )
+  end
+
+  # Pass changed contacts to partial to be able to change tr class and show transitions.
+  def changed_contacts
+    params[:ids].each_with_object({}) do |id, obj|
+      obj[id.to_i] = action_name
+    end
   end
 end
