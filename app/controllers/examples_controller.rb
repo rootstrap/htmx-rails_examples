fc5d5b243a15d96db2c7c41c3435d9d70ea07c3f
Julian Pasquale <julianagustin78@gmail.com>
2021-01-21 13:25:33 -0300

    Add delete-row example

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,11 +1,19 @@
 class ExamplesController < ApplicationController
+  before_action :set_contacts, only: [:bulk_update, :delete_row]
+
   def index; end
 
   def click_to_edit
     @contact = Contact.first
   end
 
-  def bulk_update
+  def bulk_update; end
+
+  def delete_row; end
+
+  private
+
+  def set_contacts
     @contacts = Contact.all
   end
 
